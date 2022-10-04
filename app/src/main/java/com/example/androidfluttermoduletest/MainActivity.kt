fd3b6b27c5daa110f41efec18cc2f788c85b61cb
John Qualls <altonqualls@gmail.com>
2022-10-04 16:38:18 -0500

    Add nav to flutter module

--- app/src/main/java/com/example/androidfluttermoduletest/MainActivity.kt
+++ app/src/main/java/com/example/androidfluttermoduletest/MainActivity.kt
@@ -3,26 +3,62 @@ package com.example.androidfluttermoduletest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidfluttermoduletest.ui.theme.AndroidFlutterModuleTestTheme
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidFlutterModuleTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
+                MainScreen()
+            }
+        }
+    }
+
+    @Composable
+    private fun MainScreen() {
+        // A surface container using the 'background' color from the theme
+        var isNativeScreen by remember { mutableStateOf(true) }
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (isNativeScreen) {
                     Greeting("Android")
+                    Button(onClick = { isNativeScreen = false }) {
+                        Text(text = "Navigate to flutter module")
+                    }
+                } else {
+                    CircularProgressIndicator()
+                    LocalContext.current.run {
+                        startActivity(
+                            FlutterActivity.createDefaultIntent(this)
+                        )
+                    }
                 }
             }
         }
